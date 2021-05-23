7c6bdd001557fae3badace0d97ae18762fb1b491
Felipe <bones.felipe@gmail.com>
2021-05-23 01:45:13 -0700

    Add input field and button to change alt text of currently selected text. The input field gets populated with the alt text of the currently selected image

--- V2/src/taskpane/taskpane.js
+++ V2/src/taskpane/taskpane.js
@@ -10,11 +10,35 @@ import "../../assets/icon-80.png";
 
 /* global document, Office */
 
+const parser = new DOMParser();
+
+/*
+Gets docPr tag, see: https://c-rex.net/projects/samples/ooxml/e1/Part4/OOXML_P4_DOCX_docPr_topic_ID0ES32OB.html?hl=docpr
+docPr contains the attribute "descr" which Word uses to store the alt text.
+xmlStr Is the xlm string returned by the office api.
+*/
+function getDocPr(xmlStr) {
+  let xml = parser.parseFromString(xmlStr, "application/xml");
+          
+  let tags = xml.getElementsByTagName("wp:docPr");
+  if (tags.length > 1) {
+    console.warn("Selected object with more than one cNvPr tag " + tags.length);
+  }
+
+  if (tags.length == 0) {
+    console.log("Element may not have an alt text");
+    return [null, xml];
+  }
+  
+  return [tags[0], xml];
+}
+
 Office.onReady((info) => {
   if (info.host === Office.HostType.PowerPoint || info.host === Office.HostType.Word) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
     document.getElementById("run").onclick = run;
+    document.getElementById("update-alt-text-button").onclick = updateAltText;
     document.getElementById("action").onclick = action;
     document.getElementById("hide").onclick = hide;
 
@@ -22,28 +46,46 @@ Office.onReady((info) => {
       eventArgs.document.getSelectedDataAsync(
         Office.CoercionType.Ooxml, // coercionType
         function(result) {
-          console.log("HERE!");
-          // TODO: do some decent XML parsing
-          let m = result.value.match(/descr="[a-zA-z0-9\s]+"/);
-          if (!m || m.length == 0) {
-            console.log("no alt text");
-          } else {
-            console.log(m[0]);
-            // TODO: use the decen XML parsing from above to put the 'right' alt text
-            let newxml = result.value.replace('descr="dog"', 'descr="cat"');
-            eventArgs.document.setSelectedDataAsync(newxml, { coercionType: Office.CoercionType.Ooxml }, function (asyncResult) {
-              console.log("Done!");
-              console.log(asyncResult);
-              if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-                console.error(asyncResult.error.message);
-              }
-            });
+          let altTextField = document.getElementById("curr-alt-text-input");
+          altTextField.value = "";
+          const [tag, xml] = getDocPr(result.value);
+          if (tag != null) {
+            let descr = tag.getAttribute("descr");
+            let name = tag.getAttribute("name");
+            if (descr) {
+              altTextField.value = descr;
+            }
           }
         }
       );
     });
   }
 });
+
+export async function updateAltText() {
+  Office.context.document.getSelectedDataAsync(
+    Office.CoercionType.Ooxml, // coercionType
+    function(result) {
+      const [tag, xml] = getDocPr(result.value);
+
+      if (tag == null) {
+        console.warn("This should not be null, perhaps the select element was not an image?")
+        return;
+      }
+
+      let inputAltText = document.getElementById("curr-alt-text-input").value;
+      tag.setAttribute("descr", inputAltText);
+      let newxml = (new XMLSerializer()).serializeToString(xml);
+      Office.context.document.setSelectedDataAsync(newxml, { coercionType: Office.CoercionType.Ooxml }, function (asyncResult) {
+        console.log("Done!");
+        console.log(asyncResult);
+        if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+          console.error(asyncResult.error.message);
+        }
+      });
+    }
+  );
+}
 
 export async function run() {
   /**
