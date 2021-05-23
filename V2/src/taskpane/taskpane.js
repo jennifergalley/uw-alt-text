3c5a39daa212c89c5ae9130c3829e9281a8e5d80
Felipe <bones.felipe@gmail.com>
2021-05-22 23:36:46 -0700

    Add word

--- V2/src/taskpane/taskpane.js
+++ V2/src/taskpane/taskpane.js
@@ -11,12 +11,37 @@ import "../../assets/icon-80.png";
 /* global document, Office */
 
 Office.onReady((info) => {
-  if (info.host === Office.HostType.PowerPoint) {
+  if (info.host === Office.HostType.PowerPoint || info.host === Office.HostType.Word) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
     document.getElementById("run").onclick = run;
     document.getElementById("action").onclick = action;
     document.getElementById("hide").onclick = hide;
+
+    Office.context.document.addHandlerAsync(Office.EventType.DocumentSelectionChanged, function(eventArgs) {
+      eventArgs.document.getSelectedDataAsync(
+        Office.CoercionType.Ooxml, // coercionType
+        function(result) {
+          console.log("HERE!");
+          // TODO: do some decent XML parsing
+          let m = result.value.match(/descr="[a-zA-z0-9\s]+"/);
+          if (!m || m.length == 0) {
+            console.log("no alt text");
+          } else {
+            console.log(m[0]);
+            // TODO: use the decen XML parsing from above to put the 'right' alt text
+            let newxml = result.value.replace('descr="dog"', 'descr="cat"');
+            eventArgs.document.setSelectedDataAsync(newxml, { coercionType: Office.CoercionType.Ooxml }, function (asyncResult) {
+              console.log("Done!");
+              console.log(asyncResult);
+              if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+                console.error(asyncResult.error.message);
+              }
+            });
+          }
+        }
+      );
+    });
   }
 });
 
@@ -27,7 +52,7 @@ export async function run() {
   const options = { coercionType: Office.CoercionType.Text };
 
   await Office.context.document.setSelectedDataAsync(" ", options);
-  await Office.context.document.setSelectedDataAsync("Hello World!", options);
+  await Office.context.document.setSelectedDataAsync("Hello World2!", options);
 }
 
 function hide2() {
