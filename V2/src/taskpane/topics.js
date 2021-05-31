bed9f3e29d491d073b6fe366350fcba35c65e942
Felipe <bones.felipe@gmail.com>
2021-05-31 13:35:11 -0700

    Topics.js

---
+++ V2/src/taskpane/topics.js
@@ -0,0 +1,55 @@
+// See Topic definition, each topic has a list of rules
+// each Rule has a question and a dictionary of answers, which maps to other rules and a suggestion per answer.
+// Whenever a branch reaches the end, the last rule must be END_RULE.
+const peopleTopic = new Topic("Image with people", 
+[
+    new Rule(
+        "Are the people relevant to the content?",
+        {
+            "no": { rule: END_RULE, suggestion: "Do not include it in the alt text." },
+            "yes": {
+                suggestion: "Begin by stating there are people in the image.",
+                rule: new Rule(
+                    "Is the number of people relevant to the context?",
+                    {
+                        "no": { rule: END_RULE, suggestion: "Do not include it in the alt text." },
+                        "yes": {
+                            suggestion: "Describe the total number of people.",
+                            rule: new Rule(
+                                "Is the gender/race identity relevant to the content?",
+                                {
+                                    "no": { rule: END_RULE, suggestion: "Do not disclose the gender/race identitiy of the people in the image." },
+                                    "yes": {
+                                        suggestion: "Add the general (not individual) gender/race aspects which are relevant to the context.",
+                                        rule: new Rule(
+                                            "Do you have permission to describe the people's identity?",
+                                            {
+                                                "no": { rule: END_RULE, suggestion: "Do not disclose the identities of the people." },
+                                                "yes": { rule: END_RULE, suggestion: "Describe the identity of each person in a specific order." }
+                                            }
+                                        )
+                                    }
+                                }
+                            )
+                        }
+                    }
+                )
+            }
+        }
+    )
+]
+);
+
+const decorativeTopic = new Topic("Decorative", [
+    new Rule(
+        "Is this a purely decorative image without content?",
+        {
+            "yes": { rule: END_RULE, suggestion: "Leave an empty alt text." },
+            "no": { rule: END_RULE, suggestion: "Consider choosing another category." }
+        }
+    )
+]);
+
+const TOPICS = {};
+TOPICS[decorativeTopic.name] = decorativeTopic;
+TOPICS[peopleTopic.name] = peopleTopic;
